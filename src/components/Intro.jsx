fde79145d593f6c0186dd54105c60d641b5a59e1
Tipu1996 <tipu.solehria.com>
2023-10-15 23:11:50 +0300

    img relative path correction

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -19,7 +19,7 @@ const Intro = () => {
 			</Grid>
 			<Grid item xs={3}>
 				<img
-					src="/src/imgs/devImg.jpg"
+					src="../imgs/devImg.jpg"
 					alt="devImage"
 					style={{
 						borderRadius: "50%",
