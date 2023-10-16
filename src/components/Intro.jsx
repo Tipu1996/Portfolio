078af0ae4ff77f59a5222d836b256efc591ad00e
Tipu1996 <tipu.solehria.com>
2023-10-17 02:14:25 +0300

    added profile pic

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,6 +8,17 @@ const Intro = () => {
 			justifyContent={"space-around"}
 			alignItems={"center"}
 		>
+			<Grid item xs={6} md={3}>
+				<img
+					src="profilePic.jpeg"
+					alt="Profile Pic"
+					style={{
+						borderRadius: "50%",
+						width: "100%",
+						height: "100%",
+					}}
+				/>
+			</Grid>
 			<Grid item xs={10} md={6}>
 				<Typography variant="h6" marginBottom={"3%"}>
 					Hi, I am Abdullah Ashfaq
@@ -16,17 +27,6 @@ const Intro = () => {
 					I am experienced FullStack Developer with a passion of
 					building efficient, secure and exceptional web applications
 				</Typography>
-			</Grid>
-			<Grid item xs={6} md={3}>
-				<img
-					src="devImg.jpg"
-					alt="devImage"
-					style={{
-						borderRadius: "50%",
-						width: "100%",
-						height: "100%",
-					}}
-				/>
 			</Grid>
 		</Grid>
 	);
